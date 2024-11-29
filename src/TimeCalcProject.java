40c6c1fb4430e6587a5734c7a1e553b0dc381321
devAlq <khalidalqdev@gmail.com>
2024-11-29 14:04:18 +0300

    taskDone

--- src/TimeCalcProject.java
+++ src/TimeCalcProject.java
@@ -9,10 +9,17 @@ public class TimeCalcProject {
             result += i * i;
         }
         return result;
-    }
+    }//big(n)
+    public static int sumOfSquares2(int n) {
+        return n*n*n/3 + n*n/2+ n/6;
+     }//bigO(1)
+    
 
     public static void main(String[] args) {
         int sum = sumOfSquares(10);
+        System.out.println(sum);
+
+        int sum2 = sumOfSquares2(10);
         System.out.println(sum);
     }
 }
